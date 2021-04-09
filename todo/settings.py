3a4467028b94de924367f2f93420e41db8462890
atharva hiwase <atharva.hiwase07@gmail.com>
2021-04-09 12:37:18 +0530

    inporting

--- todo/settings.py
+++ todo/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 import dj_database_url
 import django_heroku
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
