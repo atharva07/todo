13abc48bb404dba6d97e49cba8c4b3aef261a5e4
atharva hiwase <atharva.hiwase07@gmail.com>
2021-04-09 13:03:26 +0530

    new change

--- todo/settings.py
+++ todo/settings.py
@@ -130,3 +130,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
--- todo/wsgi.py
+++ todo/wsgi.py
@@ -15,18 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings.prod')
 
 application = get_wsgi_application()
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
