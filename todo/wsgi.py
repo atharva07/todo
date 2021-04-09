e34ed48478280d7476e5401a6eb6554a4dfd934d
atharva hiwase <atharva.hiwase07@gmail.com>
2021-04-09 12:58:59 +0530

    new changes

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -14,3 +14,19 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings.prod')
 
 application = get_wsgi_application()
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
