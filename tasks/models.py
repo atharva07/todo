445ac3769809aef04828f2f8da8e59cfc876c5ab
atharva hiwase <atharva.hiwase07@gmail.com>
2021-09-05 18:31:31 +0530

    new basic added

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# in this file we have created a task model to add tasks in our todo app.
+# the main function of todo app is to add different tasks which we have to do and make our 
+# priorities. 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
